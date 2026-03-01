f4d1cbf4b018093e4bce622c9b25f8c438dd5e28
rnayreed24 <RezwanAhmad.Nayreed@oulu.fi>
2026-03-01 15:46:20 +0200

    Revert "fix script error"
    This reverts commit 1677def8bf893bcd20c265831d16eccc875056dd.

--- EISim/src/main/java/com/github/hennas/eisim/agents/stackelberg/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Contains the Myopic Stackelberg game-theoretic pricing agent implementation.
- * This serves as a baseline for comparison against the DDPG pricing agent.
- */
-package com.github.hennas.eisim.agents.stackelberg;
